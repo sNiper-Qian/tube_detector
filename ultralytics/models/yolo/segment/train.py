2e043510254ebc3dd8f0398dcd8f66e158d22795
UltralyticsAssistant <web@ultralytics.com>
2026-03-17 18:03:59 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/segment/train.py
+++ ultralytics/models/yolo/segment/train.py
@@ -12,8 +12,7 @@ from ultralytics.utils.plotting import plot_results
 
 
 class SegmentationTrainer(yolo.detect.DetectionTrainer):
-    """
-    A class extending the DetectionTrainer class for training based on a segmentation model.
+    """A class extending the DetectionTrainer class for training based on a segmentation model.
 
     This trainer specializes in handling segmentation tasks, extending the detection trainer with segmentation-specific
     functionality including model initialization, validation, and visualization.
@@ -29,8 +28,7 @@ class SegmentationTrainer(yolo.detect.DetectionTrainer):
     """
 
     def __init__(self, cfg=DEFAULT_CFG, overrides: dict | None = None, _callbacks=None):
-        """
-        Initialize a SegmentationTrainer object.
+        """Initialize a SegmentationTrainer object.
 
         This initializes a trainer for segmentation tasks, extending the detection trainer with segmentation-specific
         functionality. It sets the task to 'segment' and prepares the trainer for training segmentation models.
@@ -52,8 +50,7 @@ class SegmentationTrainer(yolo.detect.DetectionTrainer):
         super().__init__(cfg, overrides, _callbacks)
 
     def get_model(self, cfg: dict | str | None = None, weights: str | Path | None = None, verbose: bool = True):
-        """
-        Initialize and return a SegmentationModel with specified configuration and weights.
+        """Initialize and return a SegmentationModel with specified configuration and weights.
 
         Args:
             cfg (dict | str, optional): Model configuration. Can be a dictionary, a path to a YAML file, or None.
